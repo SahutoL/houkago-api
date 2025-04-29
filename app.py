555f6d20b2e683d3a878d0aa84d26d67d6a95cdc
SahutoL <96646448+SahutoL@users.noreply.github.com>
2025-04-29 14:13:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def get_novel_txt(nid):
                     chapters[chapter_num] = {"error": f"チャプター{chapter_num + 1}の取得に失敗しました"}
         
         result = OrderedDict([
-            ('id', nid),
+            ('id', string(nid)),
             ('title', title),
             ('author', author),
             ('contents', chapters)
